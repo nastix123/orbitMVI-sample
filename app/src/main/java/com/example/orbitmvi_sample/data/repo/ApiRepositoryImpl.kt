b125d0cc89ed519dd86925967c7ca4a7c255f1a1
Anastasia Novikova <anastasizzzs10@gmail.com>
2024-10-02 00:07:40 +0300

    rename

--- app/src/main/java/com/example/orbitmvi_sample/data/repo/ApiRepositoryImpl.kt
+++ app/src/main/java/com/example/orbitmvi_sample/data/repo/ApiRepositoryImpl.kt
@@ -2,19 +2,18 @@ package com.example.orbitmvi_sample.data.repo
 
 import android.util.Log
 import com.example.orbitmvi_sample.data.model.PhotoDto
-import com.example.orbitmvi_sample.domain.model.Photo
+import com.example.orbitmvi_sample.domain.model.Image
 import com.example.orbitmvi_sample.domain.repo.ApiRepository
 import com.example.orbitmvi_sample.network.ApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import org.koin.core.component.KoinComponent
 
 class ApiRepositoryImpl(
     private val apiService: ApiService
 ): ApiRepository {
-    override suspend fun getPhotos(): Flow<List<Photo>> {
+    override suspend fun getPhotos(): Flow<List<Image>> {
         return flow {
             try {
                 val response = apiService.getPhotos()
@@ -34,25 +33,25 @@ class ApiRepositoryImpl(
     }
 
 
-    override suspend fun getPhoto(photoId: Int): Photo {
+    override suspend fun getPhoto(photoId: Int): Image {
         return try {
             val response = apiService.getPhoto(photoId)
             if (response.isSuccessful) {
-                response.body()?.toPhoto() ?: Photo(0, "", "")
+                response.body()?.toPhoto() ?: Image(0, "", "")
             } else {
-                Photo(0, "", "")
+                Image(0, "", "")
             }
         } catch (e: Exception) {
-            Photo(0, "", "")
+            Image(0, "", "")
         }
     }
 
 }
 
-fun PhotoDto.toPhoto(): Photo {
-    return Photo(
-        photoId = photoId,
-        photoUrl = photoUrl,
+fun PhotoDto.toPhoto(): Image {
+    return Image(
+        id = photoId,
+        url = src.original,
         photographer = photographer
     )
 }
--- app/src/main/java/com/example/orbitmvi_sample/domain/repo/ApiRepository.kt
+++ app/src/main/java/com/example/orbitmvi_sample/domain/repo/ApiRepository.kt
@@ -1,11 +1,11 @@
 package com.example.orbitmvi_sample.domain.repo
 
-import com.example.orbitmvi_sample.domain.model.Photo
+import com.example.orbitmvi_sample.domain.model.Image
 import kotlinx.coroutines.flow.Flow
 
 interface ApiRepository {
 
-    suspend fun getPhotos(): Flow<List<Photo>>
+    suspend fun getPhotos(): Flow<List<Image>>
 
-    suspend fun getPhoto(photoId: Int): Photo
+    suspend fun getPhoto(photoId: Int): Image
 }
